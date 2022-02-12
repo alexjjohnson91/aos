0519dc1b5422f0cfeea62bf6640de21d11a1c145
Alex Johnson <alexjjohnson91@gmail.com>
2022-02-12 12:40:39 -0800

    testing the a20 line

--- testA20.asm
+++ testA20.asm
@@ -2,18 +2,19 @@ testA20:
     pusha                   ; we'll just push all the registers because I am lazy
     cli                     ; halt interrupts for the duration of the procedure
 ; first address [0000:0500]
-    xor     ax, ax          ; this is for the first location
-    mov     es, ax          ; first position [0000:0500]
-    mov     si, 0x0500      ;
-    
-    xor     bl, bl          ; indirectly setting the value of the memory address
-    mov     BYTE [es:si], bl;
-    mov     dl, BYTE [es:si]; print out the number
-    call    PrintHexByte    ;
-    call    Printendl       ; 
+    mov     si, teststr_1   ; print out message
+    call    PrintString     ; 
+    call    Printendl       ;
+    mov     ax, WORD [0x7dfe]    ; this should give us the magic number
+    mov     dx, ax
+    call    PrintHexWord    ; print out the value
+    call    Printendl
 
 ; second address [ffff:0510]
-    not     ax              ; this turns on every bit in ax
+    mov     si, teststr_2   ; print out the second message
+    call    PrintString     ;
+    call    Printendl       ;
+    mov     ax, 0xffff      ; this turns on every bit in ax
     mov     ds, ax          ; set the segment register
     mov     di, 0x0510      ; address register
 
@@ -36,3 +37,5 @@ ta20_end:
 ; messages for the function
 e_disabled:     db "a20 line is disabled", 0h
 e_enabled:      db "a20 line is enabled", 0h
+teststr_1:      db "first, print the value from the first address", 0h
+teststr_2:      db "now, print the value from the second address", 0h
